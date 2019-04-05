aa9b0b6ee1a1b5b3f433cf092e124cd8931eb78c
Anton P <anton.exceedteam@gmail.com>
2019-04-05 17:36:56 +0300

    Javascript ToDo

---
+++ script_test2.js
@@ -0,0 +1,23 @@
+$(document).ready(
+    function(){
+        $(".todo_add").click(
+            function(){
+                var toAdd = $('.todo_text').val();
+                 $('ul').append(
+                 '<input class="checkbox1" type="checkbox" id="chk"> ' +
+                 '<li>' + 
+                 toAdd + 
+                 '<hr>' + 
+                 '</li>');
+            });
+            
+  
+            $("input").click(function(){
+  
+              if ($(this).is(':checked')){
+                  alert("HI");
+              } 
+              console.log('alert');
+            });
+             
+        }); 
